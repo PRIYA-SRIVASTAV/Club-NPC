bd421cb9cd1cccf91190b828543ac557f527b277
PRIYA-SRIVASTAV <priyasriofficial@gmail.com>
2023-10-27 13:08:30 +0530

    27_oct updated code

--- lib/controller/Register_controller.dart
+++ lib/controller/Register_controller.dart
@@ -3,33 +3,32 @@ import 'package:club_npc_registration/utils/helper_widget.dart';
 import 'package:flutter/material.dart';
 import '../../core/ApiCall.dart';
 
+bool isEmailValid(String email) {
+  final emailPattern = RegExp(
+    r'^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)*(\.[a-z]{2,})$',
+    caseSensitive: false,
+  );
+  return emailPattern.hasMatch(email.toString());
+}
+
 
 class RegisterController {
-  Future registerMethod(name, mobileNo, email, whoIntroduce, password,
-      cPassword, context) async {
-    if (name
-        .toString()
-        .isEmpty) {
+  Future registerMethod(
+      name, mobileNo, email, whoIntroduce, password, cPassword, context) async {
+    if (name.toString().isEmpty) {
       customFlutterToast("Name Can't be empty");
-    } else if (mobileNo
-        .toString()
-        .isEmpty) {
+    } else if (mobileNo.toString().isEmpty) {
       customFlutterToast("mobile no. can't be empty");
-    } else if (email
-        .toString()
-        .isEmpty) {
+    } else if (email.toString().isEmpty) {
       customFlutterToast("email can't be empty or email must be valid");
-    } else if (whoIntroduce
-        .toString()
-        .isEmpty) {
+    } else if (isEmailValid(email)) {
+      print("---------->${email}");
+      customFlutterToast("The email format is invalid");
+    } else if (whoIntroduce.toString().isEmpty) {
       customFlutterToast("who introduce field can't be empty");
-    } else if (password
-        .toString()
-        .isEmpty) {
+    } else if (password.toString().isEmpty) {
       customFlutterToast("Password can't be empty");
-    } else if (cPassword
-        .toString()
-        .isEmpty) {
+    } else if (cPassword.toString().isEmpty) {
       customFlutterToast("confirm password can't be empty");
     } else if (password != cPassword) {
       customFlutterToast("confirm password doesn't match");
@@ -40,14 +39,16 @@ class RegisterController {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) =>
-                Otp_page(
-                  mobile: mobileNo.toString(), fromPage: "RegisterPage",
-                ),
+            builder: (context) => Otp_page(
+              mobile: mobileNo.toString(),
+              fromPage: "RegisterPage",
+            ),
           ),
         );
       } else {
-        customFlutterToast(r["message"].toString(),);
+        customFlutterToast(
+          r["message"].toString(),
+        );
       }
     }
   }
